c8d10046681bb0c34189baf4376c7583a929868c
Tommy Thorsen <tommyt@opera.com>
2016-11-21 08:23:13 +0100

    Add a merchant that displays the event listener bug in chromium

---
+++ merchants/eventbug/inventory.js
@@ -0,0 +1,17 @@
+var items = {
+    tshirt: {
+        name: "Elegant t-shirt",
+        price: "10.00",
+        picture: "t-shirt.png"
+    },
+    wallet: {
+        name: "Genuine leather wallet",
+        price: "20.00",
+        picture: "wallet.png"
+    },
+    hat: {
+        name: "Fashionable hat",
+        price: "50.00",
+        picture: "hat.png"
+    }
+}
