ab24f98b0b6f0a88c85dc21f02a702163248ac58
Tommy Thorsen <tommyt@opera.com>
2016-12-14 15:46:05 +0100

    open window

--- payment-apps/tommypay/app.js
+++ payment-apps/tommypay/app.js
@@ -1,5 +1,8 @@
 console.log("Running service worker");
 self.addEventListener('paymentrequest', function(event) {
     console.log("PaymentRequest: " + JSON.stringify(event.data));
-    window.open("https://tommythorsen.github.io/webpayments-demo/payment-apps/tommypay/app.html");
+    windowUrl = "https://tommythorsen.github.io/webpayments-demo/payment-apps/tommypay/app.html";
+    ServiceWorkerClients.openWindow(windowUrl).then(function(WindowClient) {
+        console.log("window opened!");
+    });
 });
