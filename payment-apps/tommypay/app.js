5aa54cc05ed3568ef72f4347a165aef296601454
Tommy Thorsen <tommyt@opera.com>
2016-12-14 15:39:36 +0100

    More tommypay code

--- payment-apps/tommypay/app.js
+++ payment-apps/tommypay/app.js
@@ -1,4 +1,5 @@
 console.log("Running service worker");
 self.addEventListener('paymentrequest', function(event) {
     console.log("PaymentRequest: " + JSON.stringify(event.data));
+    window.open("https://tommythorsen.github.io/webpayments-demo/payment-apps/tommypay/app.html");
 });
