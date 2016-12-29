034367f19405422ab705b2086959a9421f0e0c6a
Tommy Thorsen <tommyt@opera.com>
2016-12-29 13:26:33 +0100

    Pass messages between service worker and page

--- payment-apps/tommypay/app.js
+++ payment-apps/tommypay/app.js
@@ -1,8 +1,23 @@
 console.log("Running service worker");
+var paymentPromise;
+
 self.addEventListener('paymentrequest', function(event) {
     console.log("PaymentRequest: " + JSON.stringify(event.data));
-    windowUrl = "https://tommythorsen.github.io/webpayments-demo/payment-apps/tommypay/app.html";
-    clients.openWindow(windowUrl).then(function(WindowClient) {
-        console.log("window opened!");
+    paymentPromise = new Promise(function(resolve, reject) {
+        windowUrl = "https://tommythorsen.github.io/webpayments-demo/payment-apps/tommypay/app.html";
+        clients.openWindow(windowUrl).then(function(windowClient) {
+            console.log("window opened!");
+            windowClient.postMessage(event.data);
+        });
     });
+    event.respondWith(paymentPromise);
+});
+
+self.addEventListener('message', function(event) {
+    console.log("PaymentResponse: " + JSON.stringify(event.data));
+    if (event.data) {
+        paymentPromise.resolve(event.data);
+    } else {
+        paymentPromise.reject();
+    }
 });
