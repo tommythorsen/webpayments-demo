4bbaef7a160ac9e7b7536215b9fcb5970e5cd556
Tommy Thorsen <tommy.thorsen@gmail.com>
2017-06-14 18:34:35 +0200

    Use event.openWindow

--- payment-apps/tommypay/pay/app.js
+++ payment-apps/tommypay/pay/app.js
@@ -21,7 +21,7 @@ self.addEventListener('paymentrequest', function(event) {
                 reject();
             }
         });
-        clients.openWindow("index.html").then(function(windowClient) {
+        event.openWindow("index.html").then(function(windowClient) {
             console.log("window opened!");
             windowClient.postMessage(event.data);
         })
